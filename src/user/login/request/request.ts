cf98953e1e4e1f1915f479d8066ab1df6b3f3856
dikac <rigel234@yahoo.com>
2019-11-04 17:53:45 +0700

    fix : add trailing path

--- src/user/login/request/request.ts
+++ src/user/login/request/request.ts
@@ -5,5 +5,5 @@ import Method from "http-method-enum";
 export default interface Request extends BaseRequest<Login> {
 
     readonly method : Method.POST;
-    readonly path : 'user/login';
+    readonly path : '/user/login';
 }

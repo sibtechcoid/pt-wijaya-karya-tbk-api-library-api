2fc56dcc63a5e6bb8cc5549330621b93f612bea7
dikac <rigel234@yahoo.com>
2019-11-06 19:32:18 +0700

    add conflict

--- src/internal/place/type/create/response/response.ts
+++ src/internal/place/type/create/response/response.ts
@@ -1,6 +1,12 @@
 import Created from "../../../../../response/created";
 import Id from "../../../../../id/id";
 import BadRequest from "../../../../../response/bad-request";
+import Conflict from "../../../../../response/conflict";
+import Message from "../../../../../message/message";
 
-type Response = BadRequest<undefined>|Created<Id<number>>;
+type Response =
+    BadRequest<undefined> |
+    Created<Id<number>> |
+    Conflict<Message>
+    ;
 export default Response;

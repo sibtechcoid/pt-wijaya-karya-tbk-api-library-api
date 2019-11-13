466e0bf7d2c10bb9d7e71e797d887981be36495e
dikac <rigel234@yahoo.com>
2019-11-13 12:03:14 +0700

    change activate to array type

--- src/activable/boolean/type.ts
+++ src/activable/boolean/type.ts
@@ -1,29 +1,11 @@
 import Activable from "../activable";
 import NotNull from "t-object/boolean/type";
+import ArrayOf from "t-array/boolean/array-of";
 import TypeInterface from "../../internal/place/type/type";
 import TypeBoolean from "t-boolean/type";
 import FiniteNumber from "t-number/boolean/finite";
 
 export default function Type(value : any) : value is Activable {
 
-    if(!NotNull<TypeInterface>(value)) {
-
-        return false;
-    }
-
-    for(let property in value) {
-
-        if(!FiniteNumber(property)) {
-
-            return false;
-        }
-
-        if(!TypeBoolean(value[property])) {
-
-            return false;
-        }
-    }
-
-    return true;
-
+    return ArrayOf(value, TypeBoolean);
 }

0245eb0b298bf17395632fe4ed8f5d6661e20042
dikac <rigel234@yahoo.com>
2019-11-15 12:41:26 +0700

    fix internal/type route

--- src/internal/place/type/read/route/route.ts
+++ src/internal/place/type/read/route/route.ts
@@ -1,8 +1,6 @@
 import Path from "../../create/path/path";
 import Method from "../method/method";
-import Type from "../../type";
 import Validator from "../../../../../validator/validator";
-import Limit from "../../../../../limit/limit";
 import Data from "../request/data/data";
 
 export default interface Route extends Path, Validator<Data>, Method {

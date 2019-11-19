2ec9dd313bbf1473a021c77b96f7e11c414fbf5a
dikac <rigel234@yahoo.com>
2019-11-20 03:55:27 +0700

    fix 'qpass/place' & add deactivated comment

--- src/qpass/place/read/response/place/place.ts
+++ src/qpass/place/read/response/place/place.ts
@@ -1,19 +1,12 @@
 import Business from "../../../business/read/response/business/business";
 import Type from "../../../level/read/response/type/type";
 import Level from "../../../type/read/response/level/level";
+import Id from "../../../../../id/id";
+import Record from "../../../../../record/record";
+import Deactivated from "../../../../../deactivated/deactivated";
 
-export default interface Place {
+export default interface Place  extends Id, Record, Deactivated{
 
-    id : number;
-
-    /**
-     * Date object compatible
-     */
-    created : string;
-    /**
-     * Date object compatible
-     */
-    updated : string;
     abbreviation : null|string;
     name : string;
     address : null|string;

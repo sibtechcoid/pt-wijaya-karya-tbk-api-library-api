e1fac57dbb31dbf19733d5ec271ce2f8ccbe33c1
dikac <rigel234@yahoo.com>
2019-11-23 05:41:36 +0700

    fix internal/position/create

--- dist/internal/position/create/request/position/create.d.ts
+++ dist/internal/position/create/request/position/create.d.ts
@@ -1,6 +1,9 @@
 import BusinessCreate from "../../../../../qpass/place/business/create/request/business/create";
 import Access from "../access/access";
-export default class Create extends BusinessCreate {
+import Position from "./position";
+import Group from "../group/group";
+export default class Create extends BusinessCreate implements Position {
     access: Access[];
-    constructor(name: string, abbreviation: string, access: Access[]);
+    group: Group;
+    constructor(name: string, abbreviation: string, access: Access[], group: Group);
 }
--- dist/internal/position/create/request/position/create.js
+++ dist/internal/position/create/request/position/create.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const create_1 = require("../../../../../qpass/place/business/create/request/business/create");
 class Create extends create_1.default {
-    constructor(name, abbreviation, access) {
+    constructor(name, abbreviation, access, group) {
         super(name, abbreviation);
         this.access = access;
+        this.group = group;
     }
 }
 exports.default = Create;

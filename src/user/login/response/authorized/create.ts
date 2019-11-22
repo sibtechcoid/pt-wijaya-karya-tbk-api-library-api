954d996bd63f29ac0b349c981ab6777031200f5f
dikac <rigel234@yahoo.com>
2019-11-22 10:06:00 +0700

    fix incorrect Authorized name

--- src/user/login/response/authorized/create.ts
+++ src/user/login/response/authorized/create.ts
@@ -1,8 +1,20 @@
 import Authorized from "./authorized";
+import Authorization from "../../../authorization/update/request/authorization/authorization";
 
 export default class Create implements Authorized {
 
     constructor(
-        public token : string
+        public token : string,
+        public authorization : Authorization,
+        public position : Position,
+        public name : string,
+        public user : string,
+        public abbreviation : string|null,
+        public phone : string|null,
+        public email : string|null,
+        public nip : string,
+        public active : boolean,
+        public positionWika : null|string,
+        public parentId : string|null,
     ) {}
 }

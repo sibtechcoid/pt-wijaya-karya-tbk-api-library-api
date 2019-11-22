1a50f0c54290f60388d740750ac046c75fd2d649
dikac <rigel234@yahoo.com>
2019-11-22 10:02:09 +0700

    fix incorrect Authorized name

--- src/user/login/response/response.ts
+++ src/user/login/response/response.ts
@@ -1,6 +1,6 @@
 import Success from "../../../response/success";
 import NotFound from "../../../response/not-found";
-import Authorized from "./token/authorized";
+import Authorized from "./authorized/authorized";
 
 type Response = NotFound<undefined>|Success<Authorized>;
 export default Response;
--- src/user/login/response/success-create.ts
+++ src/user/login/response/success-create.ts
@@ -1,4 +1,4 @@
-import Authorized from "./token/authorized";
+import Authorized from "./authorized/authorized";
 import BaseSuccess from "../../../response/success-create";
 
 export class SuccessCreate extends BaseSuccess<Authorized>  {

3190a2f3de5433c9fede715d3a8399bb7a149636
dikac <rigel234@yahoo.com>
2019-11-24 20:34:29 +0700

    fix compiler error

--- dist/internal/member/read/response/member/member.d.ts
+++ dist/internal/member/read/response/member/member.d.ts
@@ -3,6 +3,7 @@ import Id from "../../../../../id/id";
 import Action from "../../../create/request/action/action";
 import Access from "../../../../position/create/request/access/access";
 import User from "../../../../../user/read/response/user/user";
+import Position from "../../../../position/read/response/position/position";
 export default interface Member extends Record, Id {
     position: Position;
     action: Action;

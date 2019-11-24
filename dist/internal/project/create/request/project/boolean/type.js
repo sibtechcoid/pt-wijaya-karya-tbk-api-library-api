a262d7e23ad7916a96e4caced23a6af9417cb472
dikac <rigel234@yahoo.com>
2019-11-24 11:37:11 +0700

    bump version

--- dist/internal/project/create/request/project/boolean/type.js
+++ dist/internal/project/create/request/project/boolean/type.js
@@ -2,17 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const type_1 = require("t-object/boolean/type");
 const type_2 = require("t-number/boolean/type");
+const type_3 = require("t-enum/boolean/type");
 const compatible_1 = require("t-date/boolean/compatible");
+const type_4 = require("../../member/boolean/type");
+const category_1 = require("../../category/category");
 const array_of_1 = require("t-array/boolean/array-of");
 function Type(value) {
     if (!type_1.default(value)) {
         return false;
     }
-    // public planned : [Date|string, Date|string],
-    //         public realize : [Date|string, Date|string],
-    //         public place : number,
-    //         public members : Member[],
-    //         public category : Category
     if (!array_of_1.default(value.planned, compatible_1.default) && value.planned.length === 2) {
         return false;
     }
@@ -22,28 +20,12 @@ function Type(value) {
     if (!type_2.default(value.place)) {
         return false;
     }
-    if (!type_2.default(value.members)) {
-        throw new Error('implemented');
+    if (!array_of_1.default(value.members, type_4.default)) {
+        return false;
     }
-    if (!type_2.default(value.category)) {
-        throw new Error('implemented');
+    if (!type_3.default(value.category, category_1.default)) {
+        return false;
     }
-    // throw new Error('implemented');
-    // if(!IsString(value.abbreviation)) {
-    //
-    //     return false;
-    // }
-    //
-    // if(!IsString(value.address)) {
-    //
-    //     return false;
-    // }
-    //
-    // if(!IsString(value.abbreviation)) {
-    //
-    //     return false;
-    // }
-    //
     return true;
 }
 exports.default = Type;

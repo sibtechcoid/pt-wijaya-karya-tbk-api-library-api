3cc04d42c9d05203d81f8e2e44ce33fbfc77906f
dikac <rigel234@yahoo.com>
2019-11-30 06:00:53 +0700

    include compiled

--- dist/qpass/object/type/update/request/request.d.ts
+++ dist/qpass/object/type/update/request/request.d.ts
@@ -1,6 +1,6 @@
-import Body from "../../../../../body/body";
+import BaseBody from "../../../../../body/body";
 import Path from "../../create/request/path/path";
 import Method from "./method/method";
-import Data from "./body/data";
-export default interface Request extends Body<Data>, Path, Method {
+import Body from "./body/body";
+export default interface Request extends BaseBody<Body>, Path, Method {
 }

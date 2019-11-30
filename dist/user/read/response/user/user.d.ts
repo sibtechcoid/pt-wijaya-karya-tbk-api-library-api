b52f2486aee6007b079a81784e69299ac40c139e
dikac <rigel234@yahoo.com>
2019-12-01 02:14:50 +0700

    remove access from user position

--- dist/user/read/response/user/user.d.ts
+++ dist/user/read/response/user/user.d.ts
@@ -2,10 +2,10 @@ import Record from "../../../../record/record";
 import Body from "../../../../deactivate/read/response/body/body";
 import Id from "../../../../id/id";
 import Access from "../../../access/update/request/body/body";
-import Position from "../../../position/create/request/body/body";
+import Position from "../../../position/read/response/body/body";
 export default interface User extends Record, Body, Id {
     access: Access;
-    position: Position | null;
+    position: Omit<Position, 'access'> | null;
     name: string;
     user: string;
     abbreviation: string | null;

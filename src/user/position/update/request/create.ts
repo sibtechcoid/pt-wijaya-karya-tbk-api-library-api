947d4077269f6542a374d63a805cead756cbed97
dikac <rigel234@yahoo.com>
2019-12-01 11:07:58 +0700

    fix user position update

--- src/user/position/update/request/create.ts
+++ src/user/position/update/request/create.ts
@@ -1,11 +1,10 @@
 import Request from './request';
 import CreatBody from "../../../../body/create";
 import Path from "../../create/request/path/create";
-import Body from "../../create/request/body/body";
-import Id from "../../../../id/id";
+import Body from "./body/body";
 import Update from "../../../../method/update-create";
 
-export default function Create(types : Body & Id) : Request {
+export default function Create(types : Body) : Request {
 
-    return Object.assign(Update(), Path(), new CreatBody<Body & Id>(types))
+    return Object.assign(Update(), Path(), new CreatBody<Body>(types))
 }

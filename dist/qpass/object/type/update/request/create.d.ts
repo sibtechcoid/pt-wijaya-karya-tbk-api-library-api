f118799b6268884daa8ccc4ab165dea17e4eab54
dikac <rigel234@yahoo.com>
2019-12-01 08:17:10 +0700

    fix include compiled

--- dist/qpass/object/type/update/request/create.d.ts
+++ dist/qpass/object/type/update/request/create.d.ts
@@ -1,4 +1,3 @@
 import Request from './request';
-import Type from "./body/body";
-import Id from "../../../../../id/id";
-export default function Create(types: Type & Id): Request;
+import Body from "./body/body";
+export default function Create(types: Body): Request;

e935326ebed5f5a369752ad0389e6839b48e9e45
dikac <rigel234@yahoo.com>
2019-12-04 03:29:14 +0700

    fix wrong import

--- src/internal/project/create/request/body/body.ts
+++ src/internal/project/create/request/body/body.ts
@@ -1,6 +1,6 @@
 import Category from "../../../category/category";
 import Project from "../../../../../project/create/request/body/body";
-import Member from "../../../../../qpass/member/create/request/body/body";
+import Member from "../../../../../internal/member/create/request/body/body";
 
 export default interface Body extends Project<Member> {
 

1629112b87961e73fe323f8955c2d386ed5bcfcc
dikac <rigel234@yahoo.com>
2019-12-03 22:09:56 +0700

    add object update

---
+++ dist/internal/object/update/request/body/boolean/type.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const type_1 = require("t-object/boolean/type");
+const type_2 = require("t-string/boolean/type");
+const type_3 = require("t-number/boolean/type");
+const type_4 = require("../../../../../../name/update/boolean/type");
+const type_5 = require("../../../../../../abbreviation/update/boolean/type");
+const type_6 = require("../../../../../../id/boolean/type");
+function Type(value) {
+    if (!type_1.default(value)) {
+        return false;
+    }
+    if (!type_6.default(value)) {
+        return false;
+    }
+    if (!type_4.default(value)) {
+        return false;
+    }
+    if (!type_5.default(value)) {
+        return false;
+    }
+    if (value.address !== undefined) {
+        if (!type_2.default(value.address)) {
+            return false;
+        }
+    }
+    if (value.abbreviation !== undefined) {
+        if (!type_2.default(value.abbreviation)) {
+            return false;
+        }
+    }
+    if (value.business !== undefined) {
+        if (!type_3.default(value.business)) {
+            return false;
+        }
+    }
+    return true;
+}
+exports.default = Type;
+//# sourceMappingURL=type.js.map
---
+++ src/qpass/object/update/request/body/boolean/type.ts
@@ -0,0 +1,57 @@
+import TypeInterface from "../body";
+import NotNull from "t-object/boolean/type";
+import IsString from "t-string/boolean/type";
+import IsNumber from "t-number/boolean/type";
+import TypeName from "../../../../../../name/update/boolean/type";
+import TypeAbbreviation from "../../../../../../abbreviation/update/boolean/type";
+import TypeId from "../../../../../../id/boolean/type";
+
+export default function Type<Extended extends TypeInterface = TypeInterface>(value : any) : value is Extended {
+
+    if(!NotNull<Extended>(value)) {
+
+        return false;
+    }
+
+    if(!TypeId(value)) {
+
+        return false;
+    }
+
+    if(!TypeName(value)) {
+
+        return false;
+    }
+
+    if(!TypeAbbreviation(value)) {
+
+        return false;
+    }
+
+    if(value.address !== undefined) {
+
+        if(!IsString(value.address)) {
+
+            return false;
+        }
+    }
+
+    if(value.abbreviation !== undefined) {
+
+        if(!IsString(value.abbreviation)) {
+
+            return false;
+        }
+    }
+
+
+    if(value.business !== undefined) {
+
+        if(!IsNumber(value.business)) {
+
+            return false;
+        }
+    }
+
+    return true;
+}

d2e27402fc452cf8e6c4991cfaf750cc3244a0c1
dikac <rigel234@yahoo.com>
2019-12-04 00:24:01 +0700

    add type to qpass level read

--- src/qpass/object/level/read/response/body/body.ts
+++ src/qpass/object/level/read/response/body/body.ts
@@ -3,8 +3,9 @@ import Record from "../../../../../../record/record";
 import Id from "../../../../../../id/id";
 import Name from "../../../../../../name/create/name";
 import Abbreviation from "../../../../../../abbreviation/create/abbreviation";
+import Type from "../../../../../../internal/object/type/read/response/body/body";
 
 export default interface Body extends Deactivate, Record, Id, Name, Abbreviation {
 
-    // /*type : null|Omit<Type,'levels'>*/
+    type : Omit<Type,'levels'>
 }

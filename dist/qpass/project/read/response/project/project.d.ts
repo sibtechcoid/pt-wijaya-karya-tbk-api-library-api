340a6901ee7f6e28730258aeab15188438b02a71
dikac <rigel234@yahoo.com>
2019-12-03 12:37:56 +0700

    fix missing compiled data

--- dist/qpass/project/read/response/project/project.d.ts
+++
@@ -1,8 +0,0 @@
-import Object from "../../../../object/read/response/body/body";
-import Member from "../../../../member/read/response/body/body";
-import BaseProject from "../../../../../project/read/response/body/body";
-import Position from "../../../../../qpass/position/read/response/body/body";
-import Group from "../../../../position/group/group";
-import Status from "../../../status/status";
-export default interface Project extends BaseProject<Status, Group, Position, Member, Object> {
-}

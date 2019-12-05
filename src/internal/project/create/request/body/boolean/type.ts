306163bc8ea01657ff539a6c7cd7473c211e6901
dikac <rigel234@yahoo.com>
2019-12-05 18:08:52 +0700

    update project create

--- src/internal/project/create/request/body/boolean/type.ts
+++ src/internal/project/create/request/body/boolean/type.ts
@@ -2,6 +2,11 @@ import TypeInterface from "../body";
 import IsEnum from "t-enum/boolean/type";
 import Category from "../../../../category/category";
 import InternalType from "../../../../../../qpass/project/create/request/body/boolean/type";
+import Status from "../../../../status/status";
+import IsNumber from "t-number/boolean/finite";
+import ArrayOf from "t-array/boolean/array-of";
+import TypeMember from "../../../../../../member/create/request/body/boolean/type";
+import TypeDateCompatible from "t-date/boolean/compatible";
 
 export default function Type<Extended extends TypeInterface = TypeInterface>(value : any) : value is Extended {
 
@@ -10,10 +15,48 @@ export default function Type<Extended extends TypeInterface = TypeInterface>(val
         return false;
     }
 
+    if(!IsEnum(value.status, Status)) {
+
+        return false;
+    }
+
     if(!IsEnum(value.category, Category)) {
 
         return false;
     }
+
+
+    //if(value.object !== undefined){
+
+    if(!ArrayOf(value.plan, TypeDateCompatible) || value.plan.length !== 2) {
+
+        return false;
+    }
+    // }
+
+    //if(value.object !== undefined){
+
+    if(!ArrayOf(value.realize, TypeDateCompatible) || value.realize.length !== 2) {
+
+        return false;
+    }
+    // }
+
+    //if(value.object !== undefined){
+
+    if(!IsNumber(value.object)) {
+
+        return false;
+    }
+    // }
+
+    //if(value.object !== undefined){
+
+    if(!ArrayOf(value.members, TypeMember)) {
+
+        return false;
+    }
+    // }
 
     return true;
 }
--- src/qpass/project/create/request/body/body.ts
+++ src/qpass/project/create/request/body/body.ts
@@ -1,6 +1,8 @@
 import Member from "../../../../member/create/request/body/body";
 import Project from "../../../../../project/create/request/body/body";
+import Status from "../../../../../qpass/project/status/status";
 
 export default interface Body extends Project<Member> {
 
+    status : Status;
 }

f22af735af8730dbe884ad80c0734f8df24c0d18
dikac <rigel234@yahoo.com>
2019-12-05 19:12:23 +0700

    add status to qpass project creation

--- dist/qpass/project/create/request/body/boolean/type.js
+++ dist/qpass/project/create/request/body/boolean/type.js
@@ -2,33 +2,38 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const type_1 = require("t-object/boolean/type");
 const finite_1 = require("t-number/boolean/finite");
+const type_2 = require("t-enum/boolean/type");
 const compatible_1 = require("t-date/boolean/compatible");
-const type_2 = require("../../../../../../member/create/request/body/boolean/type");
+const type_3 = require("../../../../../../member/create/request/body/boolean/type");
 const array_of_1 = require("t-array/boolean/array-of");
+const status_1 = require("../../../../../../qpass/project/status/status");
 function Type(value) {
     if (!type_1.default(value)) {
         return false;
     }
-    if (value.object !== undefined) {
-        if (!array_of_1.default(value.plan, compatible_1.default) || value.plan.length !== 2) {
-            return false;
-        }
+    if (!type_2.default(value.status, status_1.default)) {
+        return false;
     }
-    if (value.object !== undefined) {
-        if (!array_of_1.default(value.realize, compatible_1.default) || value.realize.length !== 2) {
-            return false;
-        }
+    //if(value.object !== undefined){
+    if (!array_of_1.default(value.plan, compatible_1.default) || value.plan.length !== 2) {
+        return false;
     }
-    if (value.object !== undefined) {
-        if (!finite_1.default(value.object)) {
-            return false;
-        }
+    // }
+    //if(value.object !== undefined){
+    if (!array_of_1.default(value.realize, compatible_1.default) || value.realize.length !== 2) {
+        return false;
     }
-    if (value.object !== undefined) {
-        if (!array_of_1.default(value.members, type_2.default)) {
-            return false;
-        }
+    // }
+    //if(value.object !== undefined){
+    if (!finite_1.default(value.object)) {
+        return false;
     }
+    // }
+    //if(value.object !== undefined){
+    if (!array_of_1.default(value.members, type_3.default)) {
+        return false;
+    }
+    // }
     return true;
 }
 exports.default = Type;

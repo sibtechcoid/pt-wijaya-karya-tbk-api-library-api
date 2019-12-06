7d6dacfe2d9e7a05a2ecfc6539983b9dd7dfa7c6
dikac <rigel234@yahoo.com>
2019-12-06 14:36:49 +0700

    fix access

--- dist/user/read/response/user/user.d.ts
+++ dist/user/read/response/user/user.d.ts
@@ -1,7 +1,7 @@
 import Record from "../../../../record/record";
 import Deactivate from "../../../../deactivate/read/response/body/body";
 import Id from "../../../../id/id";
-import Access from "../../../access/update/request/body/body";
+import Access from "../../../access/create/request/body/body";
 import Position from "../../../position/read/response/body/body";
 export default interface Body extends Record, Deactivate, Id {
     access: Access;

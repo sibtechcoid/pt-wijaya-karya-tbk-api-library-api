094ac2b7dd2e1f0463b76a56e2777cfa1e1ee17f
dikac <rigel234@yahoo.com>
2019-12-08 23:15:17 +0700

    fix jadwal rinci response

--- dist/internal/jadwal-rinci/read/response/body/body.d.ts
+++ dist/internal/jadwal-rinci/read/response/body/body.d.ts
@@ -1,12 +1,17 @@
 import Deactivate from "../../../../../deactivate/read/response/body/body";
 import Record from "../../../../../record/record";
 import Id from "../../../../../id/id";
+import Project from "../../../../project/read/response/project/project";
+import Fungsi from "../../../fungsi/read/response/body/body";
+import Prosedur from "../../../prosedur/read/response/body/body";
+import Klausul from "../../../klausul/read/response/body/body";
+import ProsesBisnis from "../../../proses-bisnis/read/response/body/body";
 export default interface Body extends Deactivate, Record, Id {
-    project: number;
+    project: Project;
     selesai: Date | string;
     mulai: Date | string;
-    fungsi: number[];
-    prosesBisnis: number;
-    klausul: number[];
-    prosedur: number[];
+    fungsi: Fungsi[];
+    prosesBisnis: ProsesBisnis;
+    klausul: Prosedur[];
+    prosedur: Klausul[];
 }

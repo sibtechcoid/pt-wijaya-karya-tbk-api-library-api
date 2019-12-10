41bd16a26e26830c2116fbc44f86a66fb533f56e
dikac <rigel234@yahoo.com>
2019-12-10 10:43:53 +0700

    remove redundant field

--- dist/project/read/response/body/body.d.ts
+++ dist/project/read/response/body/body.d.ts
@@ -7,7 +7,7 @@ import Schedule from "../../../../schedule/read/response/body/body";
 import Name from "../../../../name/create/name";
 import Abbreviation from "../../../../abbreviation/create/abbreviation";
 import Position from "../../../../position/read/response/body/body";
-export default interface Body<MemberStatus, ProjectStatus, Group, P extends Omit<Position<any, Group>, 'access'>, M extends Omit<Member<MemberStatus, any, Group, P>, 'access'>, O extends Object<Name & Abbreviation> = Object<Name & Abbreviation>> extends Record, BaseBody, Id, Schedule {
+export default interface Body<MemberStatus, ProjectStatus, Group, P extends Position<any, Group>, M extends Omit<Member<MemberStatus, any, Group, P>, 'access'>, O extends Object<Name & Abbreviation> = Object<Name & Abbreviation>> extends Record, BaseBody, Id, Schedule {
     status: ProjectStatus;
     object: O;
     members: M[];

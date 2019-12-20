30fe88f7c86ad723626285a20ff159cacceed1d0
dikac <rigel234@yahoo.com>
2019-12-21 00:11:22 +0700

    add access documentation

--- src/user/access/internal/internal.ts
+++ src/user/access/internal/internal.ts
@@ -1,11 +1,30 @@
 enum Internal  {
+    /**
+     * update all data master
+     */
+    UPDATE = "UPDATE",
+    /**
+     * create all data master
+     */
+    CREATE = "CREATE",
+    /**
+     * read all data master
+     */
+    READ = "READ",
+    /**
+     * delete all data master
+     */
+    DELETE = "DELETE",
 
-    UPDATE = 'UPDATE',
-    CREATE = 'CREATE',
-    READ = 'READ',
-    DELETE = 'DELETE',
-    PUBLISH = 'PUBLISH',
-    APPROVE = 'APPROVE',
+    /**
+     * publish audit plan/project
+     */
+    PUBLISH = "PUBLISH",
+
+    /**
+     * approve audit plan/project, also receive publish email
+     */
+    APPROVE = "APPROVE"
 }
 
 export default  Internal;
--- src/user/access/qpass/qpass.ts
+++ src/user/access/qpass/qpass.ts
@@ -1,11 +1,34 @@
+/**
+ * Qpass Access Role
+ */
 enum Qpass  {
 
-    UPDATE = 'UPDATE',
-    CREATE = 'CREATE',
-    READ = 'READ',
-    DELETE = 'DELETE',
-    PUBLISH = 'PUBLISH',
-    APPROVE = 'APPROVE',
+    /**
+     * update all data master
+     */
+    UPDATE = "UPDATE",
+    /**
+     * create all data master
+     */
+    CREATE = "CREATE",
+    /**
+     * read all data master
+     */
+    READ = "READ",
+    /**
+     * delete all data master
+     */
+    DELETE = "DELETE",
+
+    /**
+     * publish audit plan/project
+     */
+    PUBLISH = "PUBLISH",
+
+    /**
+     * approve audit plan/project, also receive publish email
+     */
+    APPROVE = "APPROVE"
 }
 
 export default  Qpass;

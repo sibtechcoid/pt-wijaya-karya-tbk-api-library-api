c187f71140243faf23e6803ccb9eb81e3625d66e
dikac <rigel234@yahoo.com>
2019-12-24 02:01:46 +0700

    fix level usaha import

--- dist/unit-kerja/level-usaha/read/response/body/body.d.ts
+++ dist/unit-kerja/level-usaha/read/response/body/body.d.ts
@@ -3,7 +3,7 @@ import Record from "../../../../../record/record";
 import Id from "../../../../../id/id";
 import Name from "../../../../../name/create/name";
 import Abbreviation from "../../../../../abbreviation/create/abbreviation";
-import JenisUnit from "../../../../jenis-usaha/read/response/body/body";
+import JenisUnit from "../../../../jenis-unit/read/response/body/body";
 export default interface Body extends Deactivate, Record, Id, Name, Abbreviation {
     jenisUnit: Omit<JenisUnit, 'jenisUsaha' | 'levelUsaha'>;
 }

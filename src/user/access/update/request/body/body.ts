1893bdc7875cc77319f3eab7b231b7c00c28d78c
dikac <rigel234@yahoo.com>
2019-12-27 09:49:31 +0700

    add id to access update

--- src/user/access/update/request/body/body.ts
+++ src/user/access/update/request/body/body.ts
@@ -1,7 +1,8 @@
 import CreateBody from "../../../create/request/body/body";
 import {Optional} from "utility-types";
+import Id from "../../../../../id/id";
 
-export default interface Body extends Optional<CreateBody> {
+export default interface Body extends Optional<CreateBody>, Id {
 
 
 }

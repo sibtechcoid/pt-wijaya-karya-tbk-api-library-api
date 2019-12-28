ef3dbf9c4b2f9a71a7b914c68fbd4d61ecc238e4
dikac <rigel234@yahoo.com>
2019-12-29 04:27:04 +0700

    remove user access from read many

--- dist/user/read/response/body/body.d.ts
+++ dist/user/read/response/body/body.d.ts
@@ -1,11 +1,8 @@
 import Record from "../../../../record/record";
 import Deactivate from "../../../../deactivate/read/response/body/body";
 import Id from "../../../../id/id";
-import Access from "../../../access/create/request/body/body";
 import Position from "../../../position/read/response/body/body";
 export default interface Body extends Record, Deactivate, Id {
-    extraAccess: Access;
-    access: Access;
     position: Omit<Position, 'access'> | null;
     name: string;
     user: string;
--- src/user/read/response/body/body.ts
+++ src/user/read/response/body/body.ts
@@ -6,8 +6,8 @@ import Position from "../../../position/read/response/body/body";
 
 export default interface Body extends Record, Deactivate, Id {
 
-    extraAccess : Access;
-    access : Access;
+    //extraAccess : Access;
+   // access : Access;
     position : Omit<Position, 'access'>|null;
     name : string;
     user : string;

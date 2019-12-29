ba1e32926054e4431eb1e81ff2c430da5c43aaa1
dikac <rigel234@yahoo.com>
2019-12-29 07:01:43 +0700

    update sign-in response

--- dist/authentication/sign-in/response/body/body.d.ts
+++ dist/authentication/sign-in/response/body/body.d.ts
@@ -1,4 +1,7 @@
 import UserRead from "../../../../user/read/response/body/body";
+import Access from "../../../../user/access/create/request/body/body";
 export default interface Body extends UserRead {
     token: string;
+    extraAccess: Access;
+    access: Access;
 }
--- src/authentication/sign-in/response/body/body.ts
+++ src/authentication/sign-in/response/body/body.ts
@@ -1,7 +1,10 @@
 import UserRead from "../../../../user/read/response/body/body";
+import Access from "../../../../user/access/create/request/body/body";
 
 export default interface Body extends UserRead {
 
     token : string;
+    extraAccess : Access;
+    access : Access;
 
 }

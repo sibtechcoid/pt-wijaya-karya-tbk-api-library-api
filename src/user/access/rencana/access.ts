e4ec73a9aee8b36bc37805e839bcb1290e484f7b
dikac <rigel234@yahoo.com>
2019-12-30 05:23:23 +0700

    fix user access rencana

--- src/user/access/rencana/access.ts
+++ src/user/access/rencana/access.ts
@@ -1,10 +1,9 @@
-enum Access  {
-    CREATE = 'CREATE',
-    UPDATE_DATA = 'UPDATE_DATA',
-    UPDATE_PUBLISH = 'UPDATE_PUBLISH',
-    DELETE  = 'DELETE',
-    READ  = 'READ',
-    EMAIL_PUBLISH = "EMAIL_PUBLISH",
+declare enum Access {
+    CREATE = "CREATE",
+    UPDATE_DATA = "UPDATE_DATA",
+    UPDATE_SUBMIT = "UPDATE_SUBMIT",
+    DELETE = "DELETE",
+    READ = "READ",
+    EMAIL_SUBMIT = "EMAIL_SUBMIT"
 }
-
-export default  Access;
+export default Access;

a693eadfafaf16bc6531cf676dbe28aecf357244
dikac <rigel234@yahoo.com>
2019-12-30 06:47:50 +0700

    include compiled

--- dist/user/access/rencana/access.d.ts
+++ dist/user/access/rencana/access.d.ts
@@ -1,9 +1,9 @@
 declare enum Access {
     CREATE = "CREATE",
     UPDATE_DATA = "UPDATE_DATA",
-    UPDATE_PUBLISH = "UPDATE_PUBLISH",
+    UPDATE_SUBMIT = "UPDATE_SUBMIT",
     DELETE = "DELETE",
     READ = "READ",
-    EMAIL_PUBLISH = "EMAIL_PUBLISH"
+    EMAIL_SUBMIT = "EMAIL_SUBMIT"
 }
 export default Access;
--- dist/user/access/rencana/access.js
+++ dist/user/access/rencana/access.js
@@ -1,13 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Access;
-(function (Access) {
-    Access["CREATE"] = "CREATE";
-    Access["UPDATE_DATA"] = "UPDATE_DATA";
-    Access["UPDATE_PUBLISH"] = "UPDATE_PUBLISH";
-    Access["DELETE"] = "DELETE";
-    Access["READ"] = "READ";
-    Access["EMAIL_PUBLISH"] = "EMAIL_PUBLISH";
-})(Access || (Access = {}));
 exports.default = Access;
 //# sourceMappingURL=access.js.map

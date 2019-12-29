238b77eece4da562fe5f73fcd2aa21d73b4e681e
dikac <rigel234@yahoo.com>
2019-12-29 08:10:14 +0700

    recompile dist

--- src/user/read/request/create.ts
+++ src/user/read/request/create.ts
@@ -2,11 +2,10 @@ import Request from './request';
 import CreatBody from "../../../body/create";
 import Path from "../../create/request/path/create";
 import ReadCreate from "../../../method/read-create";
-import Body from "./body/body";
 import Id from "../../../id/id";
 
 
 export default function Create(id : Id) : Request {
 
-    return Object.assign(ReadCreate(), Path(), new CreatBody<Body>(id))
+    return Object.assign(ReadCreate(), Path(), new CreatBody<Id>(id))
 }

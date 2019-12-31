453ebc8499b0c2d4bde84a02d5efffe66cba53b0
dikac <rigel234@yahoo.com>
2020-01-01 03:07:11 +0700

    fix project draft

--- src/qpass/project/draft/route/create.ts
+++ src/qpass/project/draft/route/create.ts
@@ -2,7 +2,7 @@ import Route from "./route";
 import Path from "../request/path/create";
 import CreateRoute from "../../../../route/create";
 import UpdateCreate from "../../../../method/update-create";
-import TypeType from "../../update/request/body/boolean/type";
+import TypeType from "../request/body/boolean/type";
 
 export default function Create () : Route {
 

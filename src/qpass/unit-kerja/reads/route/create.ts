87d401ab3fa8b0239ec803110e2cbe91e8f589ad
dikac <rigel234@yahoo.com>
2020-01-03 18:23:53 +0700

    fix "unit kerja" reads routing argument

--- src/qpass/unit-kerja/reads/route/create.ts
+++ src/qpass/unit-kerja/reads/route/create.ts
@@ -2,7 +2,7 @@ import Route from "./route";
 import Path from "../../create/request/path/create";
 import Read from "../../../../method/read-create";
 import CreateRoute from "../../../../route/create";
-import DeactivatedType from "../../../../deactivate/read/request/body/boolean/type";
+import DeactivatedType from "../request/body/boolean/type";
 
 export default function Create () : Route {
 

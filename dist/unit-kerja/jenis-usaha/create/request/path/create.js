89a4a8b862bf522638bc7e1bc43f4b29967546ba
dikac <rigel234@yahoo.com>
2020-01-04 14:08:58 +0700

    fix jenis-usaha routing path #2 & add "Proses Bisnis" access

--- dist/unit-kerja/jenis-usaha/create/request/path/create.js
+++ dist/unit-kerja/jenis-usaha/create/request/path/create.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const create_1 = require("../../../../../path/create");
 function Create() {
-    return new create_1.default('/unit-kerja/jenisUsaha');
+    return new create_1.default('/unit-kerja/jenis-usaha');
 }
 exports.default = Create;
 //# sourceMappingURL=create.js.map

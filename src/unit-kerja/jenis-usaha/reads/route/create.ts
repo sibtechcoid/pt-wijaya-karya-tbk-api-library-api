33923f3bc12e91f0565a103a66f07eff7484aee0
dikac <rigel234@yahoo.com>
2020-01-04 13:16:25 +0700

    fix jenis-usaha routing path

--- src/unit-kerja/jenis-usaha/reads/route/create.ts
+++ src/unit-kerja/jenis-usaha/reads/route/create.ts
@@ -2,9 +2,9 @@ import Route from "./route";
 import Path from "../path/create";
 import Read from "../../../../method/read-create";
 import CreateRoute from "../../../../route/create";
-import DeactivatedType from "../../../../deactivate/read/request/body/boolean/type";
+import BodyType from "../request/body/boolean/type";
 
 export default function Create () : Route {
 
-    return CreateRoute(Read(), Path(), DeactivatedType);
+    return CreateRoute(Read(), Path(), BodyType);
 }

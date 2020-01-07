8b55696030c0c0c7827ed037dafdab5f32f00e58
dikac <rigel234@yahoo.com>
2020-01-07 09:45:59 +0700

    fix user routing path

--- dist/user/reads/request/request.d.ts
+++ dist/user/reads/request/request.d.ts
@@ -1,5 +1,5 @@
 import BaseBody from "../../../body/body";
-import Path from "../../create/request/path/path";
+import Path from "./path/path";
 import Read from "../../../method/read";
 import Body from "./body/body";
 export default interface Request extends BaseBody<Body>, Path, Read {

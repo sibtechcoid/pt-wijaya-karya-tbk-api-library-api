c37d612b9bdd129028382facfc5f22cff8ee6903
dikac <rigel234@yahoo.com>
2020-01-14 10:28:36 +0700

    fix jadwal rinci qpass routing path

--- src/audit/submit/request/create.ts
+++ src/audit/submit/request/create.ts
@@ -2,7 +2,7 @@ import Request from './request';
 import CreatBody from "../../../body/create";
 import Path from "./path/create";
 import Update from "../../../method/update-create";
-import Body from ".//body/body";
+import Body from "./body/body";
 
 export default function Create(types : Body) : Request {
 

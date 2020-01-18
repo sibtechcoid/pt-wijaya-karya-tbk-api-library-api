3b8f25eb7a28dfd788bad15d9427af2f215eafde
dikac <rigel234@yahoo.com>
2020-01-18 21:59:10 +0700

    improve user update response

--- src/user/read/response/body/body.ts
+++ src/user/read/response/body/body.ts
@@ -1,9 +1,28 @@
-import Akses from "../../../akses/create/request/body/body";
-import ReadsBody from "../../../reads/response/body/body";
+import Record from "../../../../record/record";
+import Deactivate from "../../../../deactivate/read/response/body/body";
+import Id from "../../../../id/id";
+import Jabatan from "../../../jabatan/read/response/body/body";
 
-export default interface Body extends ReadsBody {
+// export default interface Body extends ReadsBody {
+//
+//     extraAkses : Akses;
+//     akses : Akses;
+//
+// }
 
-    extraAkses : Akses;
-    akses : Akses;
+export default interface Body extends Record, Deactivate, Id {
 
+    jabatan : Omit<Jabatan, 'akses'>|null;
+    nama : string;
+    user : string;
+    singkatan : string|null;
+    telepon : string|null;
+    email : string|null;
+    nip : string|null;
+    aktif : boolean;
+    jabatanWika : null|string;
+    parentId : string|null;
+
+    //extraAkses : Akses;
+    //akses : Akses;
 }
---
+++ src/user/update-profile/response/body/body.ts
@@ -0,0 +1,6 @@
+import ReadBody from "../../../read/response/body/body";
+
+export default interface Body extends Omit<ReadBody,'extraAkses'|'akses'|'jabatan'>{
+
+
+}

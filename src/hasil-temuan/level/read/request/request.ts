19aad72ce2adcb9c618c0f60d33173b2bc707681
dikac <rigel234@yahoo.com>
2020-01-24 00:40:37 +0700

    fix hasil temuan level read request

--- src/hasil-temuan/level/read/request/request.ts
+++ src/hasil-temuan/level/read/request/request.ts
@@ -1,8 +1,8 @@
 import BaseBody from "../../../../body/body";
 import Path from "../../create/request/path/path";
 import Read from "../../../../method/read";
-import Body from "../../../../deactivate/read/request/body/body";
+import Id from "../../../../id/id";
 
-export default interface Request extends BaseBody<Body>, Path, Read {
+export default interface Request extends BaseBody<Id>, Path, Read {
 
 }

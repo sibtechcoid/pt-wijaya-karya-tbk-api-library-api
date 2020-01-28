cb22db35fe8e16a776475cfa9a7b2769bbb7b402
dikac <rigel234@yahoo.com>
2020-01-29 04:16:27 +0700

    fix penolakan property

--- dist/hasil-temuan/internal/rincian-temuan/read/response/penolakan/penolakan.d.ts
+++ dist/hasil-temuan/internal/rincian-temuan/read/response/penolakan/penolakan.d.ts
@@ -1,4 +1,4 @@
 import Body from "../body/body";
-export default interface Penolakan extends Omit<Body, 'jadwalRinci'> {
+export default interface Penolakan extends Omit<Body, 'jadwalRinci' | 'status' | 'penolakan'> {
     alasan: string;
 }
--- src/hasil-temuan/internal/rincian-temuan/read/response/penolakan/penolakan.ts
+++ src/hasil-temuan/internal/rincian-temuan/read/response/penolakan/penolakan.ts
@@ -1,6 +1,6 @@
 import Body from "../body/body";
 
-export default interface Penolakan extends Omit<Body,'jadwalRinci'> {
+export default interface Penolakan extends Omit<Body,'jadwalRinci'|'status'|'penolakan'> {
 
     alasan : string;
 }

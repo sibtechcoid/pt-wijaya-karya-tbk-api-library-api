57a74aa153bcd5522809532bc38aff83571eac9a
dikac <rigel234@yahoo.com>
2020-01-30 02:01:37 +0700

    add akses to jabatan response

--- dist/anggota/jabatan/read/response/body/body.d.ts
+++ dist/anggota/jabatan/read/response/body/body.d.ts
@@ -3,6 +3,8 @@ import Record from "../../../../../record/record";
 import CreateBody from "../../../create/request/body/body";
 import Group from "../../../group/group";
 import Deactivate from "../../../../../deactivate/read/response/body/body";
+import Akses from "../../../../akses/create/request/body/body";
 export default interface Body extends Id, Record, CreateBody, Deactivate {
+    akses: Akses;
     group: Group;
 }
--- src/anggota/jabatan/read/response/body/body.ts
+++ src/anggota/jabatan/read/response/body/body.ts
@@ -3,8 +3,10 @@ import Record from "../../../../../record/record";
 import CreateBody from "../../../create/request/body/body";
 import Group from "../../../group/group";
 import Deactivate from "../../../../../deactivate/read/response/body/body";
+import Akses from "../../../../akses/create/request/body/body";
 
 export default interface Body extends Id, Record, CreateBody, Deactivate {
 
+    akses : Akses;
     group : Group;
 }

a3be53dc29dc64df8408106b68ff893734805269
dikac <rigel234@yahoo.com>
2020-01-31 08:58:00 +0700

    remove buffer from file type

--- dist/file/create/request/body/body.d.ts
+++ dist/file/create/request/body/body.d.ts
@@ -1,9 +1,8 @@
-/// <reference types="node" />
 import UpdateBody from "../../../update/request/body/body";
 export default interface Body extends Omit<UpdateBody, 'id'> {
     /**
      * angular file buffer or node buffer
      * type cannot be forced
      */
-    data: File | Buffer;
+    data: File | any;
 }
--- src/file/create/request/body/body.ts
+++ src/file/create/request/body/body.ts
@@ -6,6 +6,6 @@ export default interface Body extends Omit<UpdateBody,'id'> {
      * angular file buffer or node buffer
      * type cannot be forced
      */
-    data : File|Buffer;
+    data : File|any;
 
 }

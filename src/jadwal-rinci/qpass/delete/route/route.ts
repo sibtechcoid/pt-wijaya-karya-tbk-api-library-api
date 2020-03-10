7b5330215ff6c9234bcc121ebe6deebf74afebdc
dikac <rigel234@yahoo.com>
2020-03-10 20:03:05 +0700

    remove jadwal rinci delete api

--- src/jadwal-rinci/qpass/delete/route/route.ts
+++
@@ -1,8 +0,0 @@
-import Path from "../../create/request/path/path";
-import Method from "../request/method/method";
-import Validator from "../../../../validator/validator";
-import Id from "../../../../id/id";
-
-export default interface Route extends Path, Validator<Id>, Method {
-
-}

5af413f7ab109620f6d0efcb30a03d4197cc70b0
dikac <rigel234@yahoo.com>
2020-03-19 19:23:09 +0700

    change hasil temuan internal confirm response

--- dist/hasil-temuan/internal/confirm/response/body/body.d.ts
+++ dist/hasil-temuan/internal/confirm/response/body/body.d.ts
@@ -1,3 +1,5 @@
-import ReadBody from "../../../read/response/body/body";
-export default interface Body extends ReadBody {
+import ReadBody from "../../../../../audit/read/response/body/body";
+import Record from "../../../../../record/record";
+import Id from "../../../../../id/id";
+export default interface Body extends Pick<ReadBody, 'fase' | keyof Record | keyof Id> {
 }
--- src/hasil-temuan/internal/confirm/response/body/body.ts
+++ src/hasil-temuan/internal/confirm/response/body/body.ts
@@ -1,5 +1,7 @@
-import ReadBody from "../../../read/response/body/body";
+import ReadBody from "../../../../../audit/read/response/body/body";
+import Record from "../../../../../record/record";
+import Id from "../../../../../id/id";
 
-export default interface Body extends  ReadBody {
+export default interface Body extends  Pick<ReadBody, 'fase' | keyof Record | keyof Id> {
 
 }

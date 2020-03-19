f6b59930952096d6737b77afe984e35ae68cac02
dikac <rigel234@yahoo.com>
2020-03-19 18:33:16 +0700

    update hasl temuan internal submit response

--- dist/hasil-temuan/internal/submit/response/body/body.d.ts
+++ dist/hasil-temuan/internal/submit/response/body/body.d.ts
@@ -1,3 +1,5 @@
-import ReadBody from "../../../read/response/body/body";
-export default interface Body extends ReadBody {
+import ReadBody from "../../../../../jadwal-rinci/internal/read/response/body/body";
+import Record from "../../../../../record/record";
+import Id from "../../../../../id/id";
+export default interface Body extends Pick<ReadBody, 'status' | keyof Record | keyof Id> {
 }
--- src/hasil-temuan/internal/submit/response/body/body.ts
+++ src/hasil-temuan/internal/submit/response/body/body.ts
@@ -1,5 +1,7 @@
-import ReadBody from "../../../read/response/body/body";
+import ReadBody from "../../../../../jadwal-rinci/internal/read/response/body/body";
+import Record from "../../../../../record/record";
+import Id from "../../../../../id/id";
 
-export default interface Body extends  ReadBody {
+export default interface Body extends  Pick<ReadBody, 'status'| keyof Record | keyof Id> {
 
 }

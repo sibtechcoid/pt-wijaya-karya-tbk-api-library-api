957b94480c2d1fe1514a3dcc4e352ee3aa4cbf10
dikac <rigel234@yahoo.com>
2020-03-21 00:01:04 +0700

    commit miss file

--- dist/hasil-temuan/internal/submit/response/body/body.d.ts
+++ dist/hasil-temuan/internal/submit/response/body/body.d.ts
@@ -1,5 +1,5 @@
 import ReadBody from "../../../../../jadwal-rinci/internal/read/response/body/body";
 import Record from "../../../../../record/record";
 import Id from "../../../../../id/id";
-export default interface Body extends Pick<ReadBody, 'status' | keyof Record | keyof Id> {
+export default interface Body extends Pick<ReadBody, 'diselesaikan' | keyof Record | keyof Id> {
 }

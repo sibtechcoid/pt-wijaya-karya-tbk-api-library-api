d999321f3f6318ab8c9a12ca227fd37cb8f9aa13
dikac <rigel234@yahoo.com>
2020-03-21 00:26:27 +0700

    remove level usaha from hasil temuan qpass response

--- dist/hasil-temuan/qpass/read/response/body/body.d.ts
+++ dist/hasil-temuan/qpass/read/response/body/body.d.ts
@@ -1,6 +1,6 @@
 import Record from "../../../../../record/record";
 import Level from "../../../../level/reads/response/body/body";
 export default interface Body extends Record {
-    level: Omit<Level, 'parent'>;
+    level: Omit<Level, 'parent' | 'levelUsaha'>;
     hasil: number;
 }
--- src/hasil-temuan/qpass/read/response/body/body.ts
+++ src/hasil-temuan/qpass/read/response/body/body.ts
@@ -3,7 +3,7 @@ import Level from "../../../../level/reads/response/body/body";
 
 export default interface Body extends Record  {
 
-    level : Omit<Level,'parent'>;
+    level : Omit<Level,'parent'|'levelUsaha'>;
     hasil : number;
 
 }

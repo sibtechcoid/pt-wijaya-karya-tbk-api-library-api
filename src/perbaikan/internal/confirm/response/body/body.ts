5ee40f5844afab7f1930de7b9b3f4dcf22721a95
dikac <rigel234@yahoo.com>
2020-03-31 04:43:26 +0700

    improve response for perbaikan internal submit, confirm

--- src/perbaikan/internal/confirm/response/body/body.ts
+++ src/perbaikan/internal/confirm/response/body/body.ts
@@ -1,6 +1,5 @@
-import Id from "../../../../../id/id";
+import Audit from "../../../../../audit/read/response/body/body";
 
-export default interface Body /*extends  ReadBody*/ {
+export default interface Body extends Pick<Audit, 'id'|'perbaikan'> {
 
-    audit : Id
 }

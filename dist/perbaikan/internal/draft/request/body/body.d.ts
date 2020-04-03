287a2082c8e12af4bd7988659ae1f0c44f341655
dikac <rigel234@yahoo.com>
2020-04-03 07:06:57 +0700

    remove partial on draft & save for perbaikan

--- dist/perbaikan/internal/draft/request/body/body.d.ts
+++ dist/perbaikan/internal/draft/request/body/body.d.ts
@@ -1,4 +1,4 @@
 import Id from "../../../../../id/id";
 import CreateBody from "../../../create/request/body/body";
-export default interface Body extends Partial<Id>, Partial<Omit<CreateBody, 'ditutup' | 'catatan'>> {
+export default interface Body extends Partial<Id>, Omit<CreateBody, 'ditutup' | 'catatan'> {
 }

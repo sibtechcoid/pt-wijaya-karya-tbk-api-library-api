35dbc75f9c98be68892e43041b9aacab87267998
dikac <rigel234@yahoo.com>
2020-04-11 15:28:11 +0700

    fix missing compiled file

--- dist/user/jabatan/create/request/body/body.d.ts
+++ dist/user/jabatan/create/request/body/body.d.ts
@@ -1,8 +1,6 @@
 import Akses from "../../../../akses/create/request/body/body";
 import Singkatan from "../../../../../singkatan/create/singkatan";
 import Nama from "../../../../../nama/create/nama";
-import Type from "../../../type/type";
 export default interface Body extends Singkatan, Nama {
     akses: Akses;
-    tipe: Type;
 }
--- dist/user/jabatan/update/request/body/body.d.ts
+++ dist/user/jabatan/update/request/body/body.d.ts
@@ -2,8 +2,6 @@ import Akses from "../../../../akses/update/request/body/body";
 import Singkatan from "../../../../../singkatan/update/singkatan";
 import Nama from "../../../../../nama/update/nama";
 import Id from "../../../../../id/id";
-import Type from "../../../type/type";
 export default interface Body extends Id, Singkatan, Nama {
-    tipe: Type;
     akses?: Akses;
 }

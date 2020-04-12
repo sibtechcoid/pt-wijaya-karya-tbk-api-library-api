f07fe22504ca10ef0697f9a05288acb27428e80b
dikac <rigel234@yahoo.com>
2020-04-12 08:05:46 +0700

    fix audit where option

--- dist/audit/reads/request/body/where/boolean/type.js
+++ dist/audit/reads/request/body/where/boolean/type.js
@@ -5,13 +5,31 @@ const type_2 = require("../../../../../../deactivate/read/request/body/boolean/t
 const type_3 = require("@dikac/t-enum/boolean/type");
 const status_1 = require("../../../../../status/status");
 const array_of_1 = require("@dikac/t-array/boolean/array-of");
+const type_4 = require("@dikac/t-array/boolean/type");
 const compatible_1 = require("@dikac/t-date/boolean/compatible");
-const type_4 = require("../../../../../type/type");
+const type_5 = require("../../../../../type/type");
 const positive_1 = require("@dikac/t-number/boolean/positive");
+const structure_1 = require("@dikac/t-object/boolean/structure");
+const undefinable_1 = require("@dikac/t-undefined/boolean/undefinable");
+const fase_1 = require("../../../../../fase/fase");
 function Type(value) {
     if (!type_1.default(value)) {
         return false;
     }
+    let count = {
+        rencana: (v) => undefinable_1.default(v, (v) => type_4.default(v) && v.length === 2),
+        realisasi: (v) => undefinable_1.default(v, (v) => type_4.default(v) && v.length === 2),
+    };
+    let sort = {
+        unitKerja: (v) => undefinable_1.default(v, positive_1.default),
+        deactivate: (v) => undefinable_1.default(v, type_2.default),
+        rencana: (v) => undefinable_1.default(v, (v) => array_of_1.default(v, compatible_1.default)),
+        realisasi: (v) => undefinable_1.default(v, (v) => array_of_1.default(v, compatible_1.default)),
+        status: (v) => undefinable_1.default(v, (v) => type_3.default(v, status_1.default)),
+        tipe: (v) => undefinable_1.default(v, (v) => type_3.default(v, type_5.default)),
+        fase: (v) => undefinable_1.default(v, (v) => type_3.default(v, fase_1.default)),
+    };
+    return structure_1.default(value, count) && structure_1.default(value, sort);
     if (!type_2.default(value)) {
         return false;
     }
@@ -26,7 +44,7 @@ function Type(value) {
         }
     }
     if (value.tipe !== undefined) {
-        if (!type_3.default(value.tipe, type_4.default)) {
+        if (!type_3.default(value.tipe, type_5.default)) {
             return false;
         }
     }
--- src/audit/reads/request/body/where/boolean/type.ts
+++ src/audit/reads/request/body/where/boolean/type.ts
@@ -4,9 +4,14 @@ import Where from "../where";
 import EnumType from "@dikac/t-enum/boolean/type";
 import Status from "../../../../../status/status";
 import ArrayOf from "@dikac/t-array/boolean/array-of";
+import ArrayType from "@dikac/t-array/boolean/type";
 import TypeDateCompatible from "@dikac/t-date/boolean/compatible";
 import AuditType from "../../../../../type/type";
 import Positive from "@dikac/t-number/boolean/positive";
+import Structure, {Validator} from "@dikac/t-object/boolean/structure";
+import Undefinable from "@dikac/t-undefined/boolean/undefinable";
+import BooleanType from "@dikac/t-boolean/type";
+import Fase from "../../../../../fase/fase";
 
 export default function
     Type<Extended extends Where = Where>(value : any
@@ -16,6 +21,25 @@ export default function
 
         return false;
     }
+
+    let count : Validator<object> = {
+        rencana: (v) => Undefinable(v, (v) : v is boolean => ArrayType(v) && v.length === 2),
+        realisasi: (v) => Undefinable(v, (v) : v is boolean => ArrayType(v) && v.length === 2),
+    };
+
+    let sort : Validator<Required<Where>> = {
+        unitKerja: (v) => Undefinable(v, Positive),
+        deactivate: (v) => Undefinable(v, DeactivatedType),
+        rencana: (v) => Undefinable(v, (v) : v is Date[] => ArrayOf(v, TypeDateCompatible)),
+        realisasi: (v) => Undefinable(v, (v) : v is Date[] => ArrayOf(v, TypeDateCompatible)),
+        status: (v) => Undefinable(v, (v) : v is Status => EnumType(v, Status)),
+        tipe: (v) => Undefinable(v, (v) : v is AuditType => EnumType(v, AuditType)),
+        fase: (v) => Undefinable(v, (v) : v is Fase => EnumType(v, Fase)),
+
+    };
+
+
+    return Structure(value, count) && Structure(value, sort);
 
     if(!DeactivatedType<Extended>(value)) {
 

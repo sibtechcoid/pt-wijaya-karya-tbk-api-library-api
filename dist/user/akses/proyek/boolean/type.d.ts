ded14f6d53bd8fc3a5f558bd6b50b0a0e66f10bf
dikac <rigel234@yahoo.com>
2020-04-18 09:50:43 +0700

    cleanup

--- dist/user/akses/proyek/boolean/type.d.ts
+++
@@ -1,2 +0,0 @@
-import Akses from "../akses";
-export default function Type(value: any): value is Akses;
--- dist/user/akses/proyek/boolean/type.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const type_1 = require("@dikac/t-enum/boolean/type");
-const akses_1 = require("../akses");
-function Type(value) {
-    return type_1.default(value, akses_1.default);
-}
-exports.default = Type;
